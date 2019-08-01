33c95f1df35df31af69a8313d5f1990e5a416c1e
MichaelHJNLee <michaelhjnlee@gmail.com>
2019-07-31 17:38:05 -0700

    hi

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,16 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  //check type of action
+    //change to first search result
+    //change on click in videoList
+  switch(action.type){
+    case 'CHANGE_VIDEO':
+      return action.video;
+    default:
+      return state;
+  }
 };
 
 export default currentVideoReducer;
